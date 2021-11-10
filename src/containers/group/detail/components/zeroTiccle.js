72a5c4fcbd4fb60822e61ac8ae21de2b941e97f3
jiwoo <hjw6112@gmail.com>
2021-11-11 00:50:45 +0900

    Component: ZeroTiccle

---
+++ src/containers/group/detail/components/zeroTiccle.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {StyleSheet, Text, ImageBackground, View, Image} from "react-native";
+import colors from '../../../../theme/colors'
+
+const  ZeroTiccle = () => {
+  return(
+    <>
+        <View style={styles.comtainer}>
+            <View style={styles.image}></View>
+            <Text style={styles.font1}>작성된 티끌이 없네요.</Text>
+            <Text style={styles.font1}>첫 티끌을 생성해보세요!</Text>
+            <View style={styles.button}>
+                <Text style={styles.font2}>저장하기</Text>
+            </View>
+        </View>
+        
+        
+    </>
+  )
+}
+
+const styles = StyleSheet.create({
+    comtainer:{
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    font1:{
+        fontSize: 16,
+        color: colors.gray4,
+    },
+    font2:{
+        fontSize: 16,
+        color: colors.white,
+    },
+    image:{
+        width:68,
+        height:68,
+        backgroundColor:colors.gray2,
+        marginBottom: 10,
+    },
+    button:{
+        backgroundColor:colors.main,
+        width:168,
+        marginTop: 30,
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical:10,
+        borderRadius: 24,
+    }
+})
+
+export default ZeroTiccle;
--- src/containers/group/detail/index.js
+++ src/containers/group/detail/index.js
@@ -4,12 +4,14 @@ import {Text} from "react-native";
 import GroupInfo from './components/groupInfo';
 import Search from './components/search';
 import Bottom from './components/bottom';
+import ZeroTiccle from './components/zeroTiccle';
 
 export default function GroupDetail(){
   return(
     <>
       <GroupInfo title={"현판"} imgUrl={'https://t1.daumcdn.net/thumb/R720x0.fpng/?fname=http://t1.daumcdn.net/brunch/service/user/8fXh/image/CyKAu5r6yUDSnRAy28UDlDEpCDs.png'} content={"데못죽 같은 거 모아두는"}/>
       <Search></Search>
+      <ZeroTiccle></ZeroTiccle>
       <Bottom></Bottom>
     </>
   )
