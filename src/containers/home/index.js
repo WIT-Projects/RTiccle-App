51f8b80367eae7625565511498d7f3ea9b4587cc
jiwoo <hjw6112@gmail.com>
2021-11-25 23:20:56 +0900

    component: myMount

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, ScrollView} from 'react-native';
+import MyMount from './components/MyMount';
+
 const Home = () => {
     return (
-      <Text>Home입니다.</Text>
+      <ScrollView>
+        <MyMount mount={"태산"} imgUrl={'https://images.france.fr/zeaejvyq9bhj/4jKuK6yobYMkGGQ4IO0Kk2/b77683922a8cca16afaa0d2028df39ad/savoie-mont-blanc-ete-header.jpg?w=1200&h=630&q=70&fl=progressive&fit=fill'}></MyMount>
+        <Text>Home입니다.</Text>
+      </ScrollView>
     );
 }
 
