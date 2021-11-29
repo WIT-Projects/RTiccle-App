7046a4a0fa301f46e9f9d74368723e853cb9401d
never-better <71776532+never-better@users.noreply.github.com>
2021-11-29 20:19:31 +0900

    EOF 추가

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import {ImageBackground, Text} from 'react-native';
+import {Text} from 'react-native';
 
 
 const Home = () => {
 
-  const imgSource = {uri :'../../assets/images/example_group.png' };
 
     return (
       <>
         <Text>Home입니다.</Text>
-        <ImageBackground source={imgSource} resizeMode = 'cover' style={{flex:1}}></ImageBackground>
       </>
     );
 }
