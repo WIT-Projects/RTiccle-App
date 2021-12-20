3f229ffdbd963381846a1c50179cf07841728263
jiwoo <hjw6112@gmail.com>
2021-12-20 11:38:27 +0900

    delete save button

--- src/containers/group/detail/components/zeroTiccle.js
+++ src/containers/group/detail/components/zeroTiccle.js
@@ -12,9 +12,6 @@ const  ZeroTiccle = () => {
         <View style={styles.image}></View>
             <Text style={styles.font1}>작성된 티끌이 없네요.</Text>
             <Text style={styles.font1}>첫 티끌을 생성해보세요!</Text>
-            <View style={styles.button}>
-                <Text style={styles.font2}>저장하기</Text>
-            </View>
         </View>
     </>
   )
@@ -30,25 +27,12 @@ const styles = StyleSheet.create({
         fontSize: 16,
         color: colors.gray4,
     },
-    font2:{
-        fontSize: 16,
-        color: colors.white,
-    },
     image:{
         width:68,
         height:68,
         backgroundColor:colors.gray2,
         marginBottom: 10,
     },
-    button:{
-        backgroundColor:colors.main,
-        width:168,
-        marginTop: 30,
-        alignItems: 'center',
-        justifyContent: 'center',
-        paddingVertical:10,
-        borderRadius: 24,
-    }
 })
 
 export default ZeroTiccle;
