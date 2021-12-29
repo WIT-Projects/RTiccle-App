d6d06129dff13e738338b474876d93b9776d2cc8
0JIEUN0 <jieun4436@naver.com>
2021-12-29 15:01:39 +0900

    Fix one word error..

--- src/containers/home/components/NewTiccleGroupList.js
+++ src/containers/home/components/NewTiccleGroupList.js
@@ -23,7 +23,9 @@ const NewTiccleGroupList = () => {
             <Text style={styles.blackBoldFont}>신규 티끌이 생성된 그룹</Text>
             {isExistGroup ? 
                 <View style={styles.container} >
-                    {data.map((item, index) => {return (<TiccleGroup key={index} imgUrl={item.imageUrl} groupTitle={item.title} ticcleTitle={'ticcleTitle'}></TiccleGroup>)})}
+                    {data.map((item, index) => {
+                        return (<TiccleGroup key={index} imageUrl={item.imageUrl} groupTitle={item.title} ticcleTitle={'ticcleTitle'}></TiccleGroup>)
+                    })}
                 </View> 
                 : <Text>작성된 티끌이 존재하지 않습니다.</Text>
             }
