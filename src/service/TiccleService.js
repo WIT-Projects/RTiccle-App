93b8e123abad859cacd420a36f887ca805c113e5
0JIEUN0 <jieun4436@naver.com>
2022-01-04 21:51:38 +0900

    Add support to ticcleNum of group and update/delete ticcle

--- src/service/TiccleService.js
+++ src/service/TiccleService.js
@@ -1,6 +1,7 @@
 import firestore from '@react-native-firebase/firestore';
 import { getCurrentUser } from './AuthService';
-import { uploadImageToStorage, getDownloadURLByName } from './ImageService';
+import { uploadImageToStorage, getDownloadURLByName, deleteImageFromStorage } from './ImageService';
+import { updateGroupInfo, updateTiccleNumOfGroup } from './GroupService';
 
 const user = getCurrentUser();
 const userDoc = firestore().collection('RTiccle').doc(user.uid);
@@ -25,6 +26,7 @@ async function createTiccle(newTiccle) {
         link: String, // URL of original content
         content: String,
         tagList: Array<String>
+        // images
     }
  * @param {Array} images: image source array - LIMIT 2
  * @returns {Promise<String>} created ticcle id (random)
@@ -41,8 +43,82 @@ function uploadNewTiccle(ticcle, images) {
         })
     }
 
+    // update group info (ticcleNum + 1)
+    updateTiccleNumOfGroup(ticcle.group, true);
     // upload ticcle info
     return createTiccle({ ...ticcle, images: imageArr });
+}
+
+
+/**
+ * Update ticcle info (no support for image)
+ * @param {string} groupId: original group id ticcle belong to
+ * @param {string} ticcleId
+ * @param {Array} newInfo: new ticcle info (changed info only)
+ * @param {string} newGroupId: new group id if group changed (else null)
+ * Support info:
+ * *  {
+        title: String,
+        link: String, // URL of original content
+        content: String,
+        tagList: Array<String>
+    }
+ */
+function updateTiccleInfo(groupId, ticcleId, newInfo, newGroupId) {
+    const ref = userDoc.collection("Ticcle").doc(ticcleId);
+    var updateInfo = {...newInfo, lastModifiedTime: FBDate()};
+
+    // update group info
+    if (newGroupId) { // if change group
+        updateTiccleNumOfGroup(groupId, false);
+        updateTiccleNumOfGroup(newGroupId, true);
+        updateInfo = {...updateInfo, group: newGroupId};
+    } else { // group not changed
+        updateGroupInfo(groupId); // only update lastModifiedDate!
+    }
+    ref.update(updateInfo);
+}
+
+/**
+ * Update ticcle images
+ * @param {string} groupId 
+ * @param {string} ticcleId 
+ * @param {Array} oldImages old images array // if not exists, put []
+ * @param {string} oldImageName old image name // if not exists, put null
+ * @param {*} newImageSource new image source
+ * @returns newImageName
+ */
+function updateTiccleImage(groupId, ticcleId, oldImages, oldImageName, newImageSource) {
+    var images = [...oldImages];
+    // delete original image first
+    if (oldImageName) {
+        deleteImageFromStorage(oldImageName, true);
+        let idx = images.indexOf(oldImageName);
+        images.splice(idx, 1);
+    }
+    // upload new image
+    newImageName = Date.now() + ".jpg";
+    uploadImageToStorage(newImageName, newImageSource);
+    images.push(newImageName);
+    // update group info
+    updateTiccleInfo(groupId, ticcleId, {images: images});
+    return newImageName;
+}
+
+/**
+ * Delete ticcle 
+ * @param {Array} ticcle: ticcle info (MUST include 'id', 'group', 'images' information)
+ */
+function deleteTiccle(ticcle) {
+    // delete images
+    if (group.images.length > 0) {
+        group.images.forEach((imageName) => deleteImageFromStorage(imageName, true));
+    }
+    // delete group info
+    const ref = userDoc.collection("Ticcle").doc(ticcle.id);
+    ref.delete();
+    // update group info (ticcleNum - 1)
+    updateTiccleNumOfGroup(ticcle.group, false);
 }
 
 /**
