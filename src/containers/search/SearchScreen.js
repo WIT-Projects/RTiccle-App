2ca716cd539238ae29b6d4431d44177bce19d0e3
jiwoo <hjw6112@gmail.com>
2022-01-04 23:39:43 +0900

    TypeBar

--- src/containers/search/SearchScreen.js
+++ src/containers/search/SearchScreen.js
@@ -2,11 +2,13 @@ import React from 'react';
 import {Text, StyleSheet} from 'react-native';
 import colors from '../../theme/colors';
 import SearchBar from '../common/SearchBar';
+import TypeBar from './components/TypeBar';
 
 const SearchScreen = () => {
     return (
         <>
             <SearchBar placeholderContext="제목, #태그 등을 입력하세요."/>
+            <TypeBar/>
         </>
     );
 };
