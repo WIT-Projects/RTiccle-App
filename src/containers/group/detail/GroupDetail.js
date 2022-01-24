9c39fcd48f5e96a6ec870c767775c92d7694a581
jiwoo <hjw6112@gmail.com>
2022-01-24 13:35:48 +0900

    change file name

--- src/containers/group/detail/GroupDetail.js
+++ src/containers/group/detail/GroupDetail.js
@@ -1,8 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import {Image, TouchableOpacity, StyleSheet} from 'react-native';
-import GroupInfo from './components/groupInfo';
+import GroupInfo from './components/GroupInfo';
 import SearchBar from '../../common/SearchBar';
-import ZeroTiccle from './components/zeroTiccle';
+import ZeroTiccle from './components/ZeroTiccle';
 import GroupDetailTiccleList from './components/GroupDetailTiccleList';
 import useTiccleChanged from '../../../context/hook/useTiccleChanged';
 import {getTiccleListByGId} from '../../../model/TiccleModel';
