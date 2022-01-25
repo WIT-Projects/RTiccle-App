1799ad180f626ee5df71cf7bd150c4fd00bb1eef
0JIEUN0 <jieun4436@naver.com>
2022-01-25 20:50:57 +0900

    Add support for ticcle search in Group (local search)

--- src/service/SearchService.js
+++ src/service/SearchService.js
@@ -2,14 +2,16 @@ import firestore from '@react-native-firebase/firestore';
 import { currentUser } from './AuthService';
 
 const algoliasearch = require("algoliasearch");
-var searchClient;
 var collectionIndex;
 const userFilter = `uid:${currentUser.uid}`
 
 async function initAlgolia() {
+    // Get Algolia keys first
     const doc = await firestore().collection('Algolia').doc('algolia').get();
     const algolia = doc.data();
-    searchClient = algoliasearch(algolia.AppId, algolia.APIKey);
+
+    // Generate Algolia client and target index
+    const searchClient = algoliasearch(algolia.AppId, algolia.APIKey);
     const indexName = 'dev_ticcletitle';
     collectionIndex = searchClient.initIndex(indexName);
 };
@@ -44,7 +46,38 @@ async function initAlgolia() {
     });
 }
 
+/**
+ * Search ticcle by title (in group)
+ * @param {string} query 
+ * @param {Array} ticcleList ticcle list in same group (MUST CONTAIN "title")
+ * @returns 
+ */
+function searchTiccleByTitleInGroup(query, ticcleList) {
+    const result = ticcleList.filter((ticcle) => {
+        if (ticcle.title.indexOf(query) < 0) return false;
+        else return true;
+    });
+    return result;
+}
+
+/**
+ * Search ticcle by tag (in group)
+ * @param {string} query 
+ * @param {Array} ticcleList ticcle list in same group (MUST CONTAIN "tagList")
+ * @returns 
+ */
+function searchTiccleByTagInGroup(query, ticcleList) {
+    const result = ticcleList.filter((ticcle) => {
+        if (ticcle.tagList.find(val => val == query) == undefined) return false;
+        // if (JSON.stringify(ticcle.tagList).indexOf(query) < 0) return false; // if not exact match
+        else return true;
+    });
+    return result;
+}
+
 export {
     initAlgolia,
     searchTiccleWithAlgolia,
+    searchTiccleByTitleInGroup,
+    searchTiccleByTagInGroup,
 };
