95a7dd322803594338773f799022b255a8100308
YoonJeon <yoonjeon0413@gmail.com>
2022-02-02 04:18:16 +0900

    Modify errorFallback design

--- src/containers/common/ErrorFallback.js
+++ src/containers/common/ErrorFallback.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import {View, TouchableOpacity, Text, StyleSheet, BackHandler} from 'react-native';
+import {View, TouchableOpacity, Text, StyleSheet, Image} from 'react-native';
 import colors from '../../theme/colors';
 import {type} from '../../theme/fonts';
-import { restartApp } from '../../service/CommonService';
+import {restartApp} from '../../service/CommonService';
 
-const ErrorFallback = ({error}) =>
-{
-   
-
+const ErrorFallback = () => {
     return (
         <View style={styles.container}>
-            <Text style={styles.errorText}>네트워크 연결에 문제가 생겼어요.</Text>
-            <Text style={styles.errorText}>개발자 연락처: rticcle@gmail.com</Text>
-            <Text style={[styles.errorText, styles.red]}>Error : {error.message}</Text>
-            <TouchableOpacity style={styles.restartButton} onPress={()=> restartApp()}>
+            <View style={styles.info}>
+                <Image source={require('../../assets/images/errorFallbackLogo.png')} style={styles.logo}></Image>
+                <Text style={styles.infoText}>예상치 못한 문제가 발생했어요.</Text>
+                <Text style={styles.infoText}>네트워크 연결 상태를 확인해주세요.</Text>
+            </View>
+            <TouchableOpacity style={styles.restartButton} onPress={() => restartApp()}>
                 <Text style={styles.restartButtonText}>앱 재시작</Text>
             </TouchableOpacity>
+            <View style={styles.report}>
+                <Text style={styles.reportText}>만일 네트워크 문제가 아니라면,</Text>
+                <Text style={styles.reportText}>오류 내용을 제보해주시면 감사하겠습니다.</Text>
+                <Text style={styles.reportText}>개발자 계정: rticcle@gmail.com</Text>
+            </View>
         </View>
     );
 };
@@ -25,26 +29,49 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: colors.white,
         alignItems: 'center',
-        justifyContent:'center'
+        justifyContent: 'center',
     },
-    errorText: {
+    info: {
+      alignItems:'center'
+    },
+    infoText: {
         fontFamily: type.notoSansKR_Regular,
+        color: colors.main,
         fontSize: 16,
         lineHeight: 26,
     },
-    red: {
-        color: colors.red,
+    logo: {
+        width: 55,
+        height: 35,
+        marginBottom: 32,
     },
     restartButton: {
-        marginTop: 50,
+        width: 168,
+        height: 40,
+        justifyContent: 'center',
+        alignItems:'center',
+        marginTop: 74,
         backgroundColor: colors.main,
-        paddingHorizontal: 50,
-        paddingVertical: 10,
-        borderRadius:24
+        borderRadius: 24,
     },
     restartButtonText: {
+        fontFamily: type.notoSansKR_Regular,
         color: colors.white,
-    }
+        fontSize: 16,
+        lineHeight:20
+    },
+    report: {
+        position: 'absolute',
+        bottom: 0,
+        alignItems: 'center',
+        paddingBottom: 22,
+    },
+    reportText: {
+        fontFamily: type.notoSansKR_Regular,
+        color: colors.gray3,
+        fontSize: 14,
+        lineHeight: 22,
+    },
 });
 
 export default ErrorFallback;
